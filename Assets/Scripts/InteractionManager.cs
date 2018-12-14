bcb2a909d0e4d2e076d60ffc27f27d80821d3873
Arkadeep <M1044398@mindtree.com>
2018-12-14 11:40:22 +0530

    initial commit

---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InteractionManager : MonoBehaviour {
+
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+    [DllImport("__Internal")]
+    private static extern void HelloString(string str);
+
+    [DllImport("__Internal")]
+    private static extern void PrintFloatArray(float[] array, int size);
+
+    [DllImport("__Internal")]
+    private static extern int AddNumbers(int x, int y);
+
+    [DllImport("__Internal")]
+    private static extern string StringReturnValueFunction();
+
+    [DllImport("__Internal")]
+    private static extern void BindWebGLTexture(int texture);
+
+    void Start()
+    {
+        //Hello();
+
+        //HelloString("This is a string.");
+
+        //float[] myArray = new float[10];
+        //PrintFloatArray(myArray, myArray.Length);
+
+        //int result = AddNumbers(5, 7);
+        //Debug.Log(result);
+
+        //Debug.Log(StringReturnValueFunction());
+
+        //var texture = new Texture2D(0, 0, TextureFormat.ARGB32, false);
+        //BindWebGLTexture(texture.GetNativeTextureID());
+    }
+
+    public void OnMouseDown()
+    {
+        HelloString("This is a string.");
+        Debug.Log("CubeClicked");
+    }
+
+    public void Message(string message)
+    {
+        this.gameObject.transform.position = new Vector3(3, 3, 3);
+    }
+}
